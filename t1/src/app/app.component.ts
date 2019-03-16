b478dbdf728a55ee7c77ecacb7039bb618ba14d8
greivin24 <31831690+greivin24@users.noreply.github.com>
2019-03-16 15:49:33 -0600

    Local Storage

--- t1/src/app/app.component.ts
+++ t1/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataStorageService } from './data-storage.service';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 't1';
+
+  constructor(private dataStorageService:DataStorageService){
+
+    this.dataStorageService.setObjectValue("nombre", "Greivin RoAt");
+
+    console.log(this.dataStorageService.getObjectValue("nombre"));
+  }
+
+
 }
--- t1/src/app/app.module.ts
+++ t1/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { DataStorageService } from './data-storage.service';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { environment } from '../environments/environment';
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
-  providers: [],
+  providers: [
+    DataStorageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
